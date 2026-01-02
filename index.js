c9c449cd0c13caa1ade6e9f0ddf08511e9a3f1e8
gauravsingh001-cyber <gauravsinghrajput292@gmail.com>
2026-01-02 17:45:59 +0530

    githubdata added

--- index.js
+++ index.js
@@ -5,6 +5,42 @@ const express = require('express')
 const app = express()
 const port = 4000
 
+const githubdata ={
+  "login": "hiteshchoudhary",
+  "id": 11613311,
+  "node_id": "MDQ6VXNlcjExNjEzMzEx",
+  "avatar_url": "https://avatars.githubusercontent.com/u/11613311?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/hiteshchoudhary",
+  "html_url": "https://github.com/hiteshchoudhary",
+  "followers_url": "https://api.github.com/users/hiteshchoudhary/followers",
+  "following_url": "https://api.github.com/users/hiteshchoudhary/following{/other_user}",
+  "gists_url": "https://api.github.com/users/hiteshchoudhary/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/hiteshchoudhary/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/hiteshchoudhary/subscriptions",
+  "organizations_url": "https://api.github.com/users/hiteshchoudhary/orgs",
+  "repos_url": "https://api.github.com/users/hiteshchoudhary/repos",
+  "events_url": "https://api.github.com/users/hiteshchoudhary/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/hiteshchoudhary/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": "Hitesh Choudhary",
+  "company": null,
+  "blog": "https://hitesh.ai",
+  "location": "India",
+  "email": null,
+  "hireable": null,
+  "bio": "I make coding videos on youtube and for courses. My youtube channel explains my work more. Check that out",
+  "twitter_username": "hiteshdotcom",
+  "public_repos": 116,
+  "public_gists": 5,
+  "followers": 53333,
+  "following": 0,
+  "created_at": "2015-03-23T13:03:25Z",
+  "updated_at": "2025-12-10T16:57:39Z"
+}
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -16,6 +52,11 @@ app.get('/twitter', (req, res) =>{
 app.get('/login',(req,res)=>{
     res.send('<h1> please login at chai aur code </h1>')
 })
+
+app.get('/github',(req,res) => {
+  res.json(githubdata)
+})
+
 app.get('/youtube',(req,res)=>{
     res.send("<h2>Chai aur coding kroo</h2>")
 })
